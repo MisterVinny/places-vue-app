5cce5453f87030b1b6371fa8978b529cbf81d60c
vinnyk <ragdolleye@gmail.com>
2021-06-07 22:22:04 -0500

    Adds Home.vue, adds placesIndex function, starts placesCreate function.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,58 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+
+    <div>
+      <input v-model="placesParams.name" type="text" />
+      <input v-model="placesParams.address" type="text" />
+      <button v-on:click="createPlace()">Submit Place</button>
+      <p>{{ placesParams }}</p>
+    </div>
+
+    <div v-for="place in places" v-bind:key="place.id">
+      <p><b>Name:</b> {{ place.name }}</p>
+      <p><b>Address:</b> {{ place.address }}</p>
+      <hr />
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+p b {
+  font-style: bold;
+}
+</style>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Good stuff in Chicago!",
+      places: [],
+      placesParams: {},
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.placesIndex();
+  },
+  methods: {
+    placesIndex: function () {
+      axios.get("http://localhost:3000/places/").then((response) => {
+        console.log(response.data);
+        this.places = response.data;
+      });
+    },
+
+    placesCreate: function (placesParams) {
+      axios
+        .post("http://localhost:3000/places/")
+        .then((response) => {
+          console.log(response.data);
+          this.places.push(response.data);
+        })
+        .catch();
+    },
+  },
 };
 </script>
